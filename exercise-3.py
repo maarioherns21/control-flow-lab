55cce333aa00e864b1b5dce082cf9d316a282b36
Mario Hernandez <mariohernandez@Marios-MacBook-Pro.local>
2022-04-19 17:00:08 -0700

    2 more problems

--- exercise-3.py
+++ exercise-3.py
@@ -3,10 +3,17 @@
 # Write the code that:
 # 1. Prompts the user to enter a dog's age in human years like this:
 #      Input a dog's age in human years: 
+human_years = int(input("Input a dog's age in human years: "))
 # 2. Calculates the equivalent dog years, where:
-#      - The first two years count as 10 years each
-#      - Any remaining years count as 7 years each
-# 3. Prints the answer in the following format:
+while human_years != "quit":
+    #      - The first two years count as 10 years each
+    if human_years < 3:
+     dog_years = human_years * 10
+     #      - Any remaining years count as 7 years each
+    else:
+     dog_years = 20 + (human_years - 2) * 7
+     # 3. Prints the answer in the following format:
 #      The dog's age in dog years is xx
-
+    print(f"The dog's age in dog years is {dog_years}")
+    human_years = int(input("Input a dog's age in human years: "))
 # Hint:  Use the int() function to convert the string returned from input() into an integer
--- exercise-4.py
+++ exercise-4.py
@@ -3,12 +3,23 @@
 # Write the code that:
 # 1. Prompts the user to enter the three lengths of a triangle (one at a time):
 #      Enter the lengths of three side of a triangle:
-#      a: 
-#      b:
-#      c:
-# 2. Write the code that determines if the triangle is:
+print('Enter the lengths of three side of a triangle:')
+a = input('a: ')
+b = input('b: ')
+c = input('c: ')
+# while loop 
+while a != "quit":
 #      equalateral - all three sides are equal in length
+    if a == b and b == c:
+        print(f"the sides are: {a} {b} {c} all three sides are equal in length so is a equalateral")
 #      scalene - all three sides are unequal in length
+    elif a != b and a != c and b != c:
+        print(f"the sides are: {a} {b} {c} all three sides are unequal in length so is a scalane")
 #      isosceles - two sides are the same length
-# 3. Print a message such as:
-#      - A triangle with sides of <a>, <b> & <c> is a <type of triangle> triangle
+    else:
+        print(f"the sides are:{a} {b} {c} two sides are the same length so is a isosceles")
+#    loooping    
+    print('Enter the lengths of three side of a triangle:')
+  
+
+

43f5578b22bccd96c9c4b2f18f26a6d4c6d97a84
Mario Hernandez <mariohernandez@Marios-MacBook-Pro.local>
2022-04-20 09:13:39 -0700

    update

--- exercise-4.py
+++ exercise-4.py
@@ -8,18 +8,18 @@ a = input('a: ')
 b = input('b: ')
 c = input('c: ')
 # while loop 
-while a != "quit":
+
 #      equalateral - all three sides are equal in length
-    if a == b and b == c:
+if a == b and b == c:
         print(f"the sides are: {a} {b} {c} all three sides are equal in length so is a equalateral")
 #      scalene - all three sides are unequal in length
-    elif a != b and a != c and b != c:
+elif a != b and a != c and b != c:
         print(f"the sides are: {a} {b} {c} all three sides are unequal in length so is a scalane")
 #      isosceles - two sides are the same length
-    else:
+else:
         print(f"the sides are:{a} {b} {c} two sides are the same length so is a isosceles")
 #    loooping    
-    print('Enter the lengths of three side of a triangle:')
+
   
 
 
